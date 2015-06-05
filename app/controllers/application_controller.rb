a82e4938e871f75307de6616aacfa2a97800b227
Dakota Carter <dakotacar@gmail.com>
2015-06-05 02:48:40 +0000

    force http instead of https

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,11 @@
 
 
 class ApplicationController < ActionController::Base
+  before_filter do
+    if request.ssl? && Rails.env.production?
+      redirect_to :protocol => 'http://', :status => :moved_permanently
+    end
+  end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
